987d2ad437aec02560874915d9cfb796b21bbd80
havstad <oystein.havstad@gmail.com>
2013-03-15 15:56:59 +0100

    12334

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+
+public class HelloWorld {
+	
+	public static void main(String[] args) throws Exception {
+		
+		while( ( true || false ) && (2 + 2) == 4) {
+			System.out.println("Hello World");
+			
+			Thread.sleep(100);    
+			
+	 		//  
+		}
+		
+	}
+	
+}
